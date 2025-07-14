27424e24fc92332800370b229f9d3631e96a04f2
Olatunji Lawal <lawaltreasure08@gmail.com>
2025-07-14 18:27:14 +0100

    dashboard sidebar

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { MdDashboard } from "react-icons/md";
 
 const DashboardSidebar = () => {
   return (
-    <div className='h-40 mb-3 lg:w-[20%] md:w-[35%] bg-white rounded-lg hidden md:block'></div>
-  )
-}
+    <div className="h-fit mb-3 lg:w-[20%] py-4 pl-5 md:w-[35%] bg-white rounded-lg hidden md:block">
+      <ul className="flex flex-col gap-2">
+        <li className="flex items-center gap-5">
+          <MdDashboard size={30} />
+          <p>Dashboard</p>
+        </li>
+        <li className="flex items-center gap-5">Orders</li>
+        <li className="flex items-center gap-5">Products</li>
+        <li className="flex items-center gap-5">Payments</li>
+        <li className="flex items-center gap-5">Settings</li>
+      </ul>
+    </div>
+  );
+};
 
-export default DashboardSidebar
+export default DashboardSidebar;
